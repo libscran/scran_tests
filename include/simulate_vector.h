a41d9d9bd480d98a8de59e06c218ce587d7e670d
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-06-24 14:56:26 -0700

    Avoid surprises during template deduction for simulate_vector.

--- include/simulate_vector.h
+++ include/simulate_vector.h
@@ -5,8 +5,8 @@
 #include <vector>
 #include <cstdint>
 
-template<typename Float_>
-std::vector<Float_> simulate_vector(size_t length, Float_ lower = -10, Float_ upper = 10, uint64_t seed = 1234567890) {
+template<typename Float_ = double>
+std::vector<Float_> simulate_vector(size_t length, double lower = -10, double upper = 10, uint64_t seed = 1234567890) {
     std::mt19937_64 rng(seed);
     std::uniform_real_distribution<Float_> unif(lower, upper);
     std::vector<Float_> values(length);
@@ -16,8 +16,8 @@ std::vector<Float_> simulate_vector(size_t length, Float_ lower = -10, Float_ up
     return values;
 }
 
-template<typename Float_>
-std::vector<Float_> simulate_sparse_vector(size_t length, Float_ density, Float_ lower = -10, Float_ upper = 10, uint64_t seed = 1234567890) {
+template<typename Float_ = double>
+std::vector<Float_> simulate_sparse_vector(size_t length, double density, double lower = -10, double upper = 10, uint64_t seed = 1234567890) {
     std::mt19937_64 rng(seed);
     std::uniform_real_distribution<Float_> nonzero(0.0, 1.0);
     std::uniform_real_distribution<Float_> unif(lower, upper);
